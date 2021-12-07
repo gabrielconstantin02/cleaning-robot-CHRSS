605dbc7293ce9f7e7df1009cba44af013363c1cc
Sociu Daniel <danielsociu00@gmail.com>
2021-12-07 14:59:35 +0200

    Updated socketio

--- cleaning-robot/__init__.py
+++ cleaning-robot/__init__.py
@@ -105,8 +105,40 @@ def background_thread():
 def run_socketio_app():
     create_app()
     create_mqtt_app()
-    socketio.run(app, host='localhost', port=5000, use_reloader=False, debug=True)
+    socketio.run(app, host='localhost', port=5001, use_reloader=False, debug=True)
+
+    @socketio.on('publish')
+    def handle_publish(json_str):
+        data = json.loads(json_str)
+        mqtt.publish(data['topic'], data['message'])
+
+
+    @socketio.on('subscribe')
+    def handle_subscribe(json_str):
+        data = json.loads(json_str)
+        mqtt.subscribe(data['topic'])
+
+
+    @socketio.on('unsubscribe_all')
+    def handle_unsubscribe_all():
+        mqtt.unsubscribe_all()
+
+
+    @mqtt.on_message()
+    def handle_mqtt_message(client, userdata, message):
+        data = dict(
+            topic=message.topic,
+            payload=message.payload.decode()
+        )
+        socketio.emit('mqtt_message', data=data)
+
+
+    @mqtt.on_log()
+    def handle_logging(client, userdata, level, buf):
+        print(level, buf)
+
 
 
 if __name__ == '__main__':
     run_socketio_app()
+
