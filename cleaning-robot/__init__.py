d1272ce1cf99e7ab5daa0293961dfe79a091cbd8
Sociu Daniel <danielsociu00@gmail.com>
2021-12-07 14:15:12 +0200

    Added base MQTT

--- cleaning-robot/__init__.py
+++ cleaning-robot/__init__.py
@@ -1,6 +1,12 @@
 import os
-
 from flask import Flask
+from flask_mqtt import Mqtt
+from flask_socketio import SocketIO
+from threading import Thread
+import json
+import time
+import eventlet
+
 from . import db
 from . import auth
 from . import environment
@@ -8,10 +14,19 @@ from . import cleaning
 from . import vacuum_settings
 from . import mop_settings
 from . import cleaning_schedule
+from . import status
+
+app = None
+mqtt = None
+socketio = None
+thread = None
+
+eventlet.monkey_patch()
 
 def create_app(test_config=None):
 
     # create and configure the app
+    global app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
@@ -36,6 +51,11 @@ def create_app(test_config=None):
    # a simple page that says hello
     @app.route('/hello')
     def hello():
+        global thread
+        if thread is None:
+            thread = Thread(target=background_thread)
+            thread.daemon = True
+            thread.start()
         return 'Hello, World!'
 
     app.register_blueprint(auth.bp)
@@ -46,3 +66,47 @@ def create_app(test_config=None):
     app.register_blueprint(cleaning_schedule.bp)
 
     return app
+
+def create_mqtt_app():
+    # Setup connection to mqtt broker
+    app.config['MQTT_BROKER_URL'] = 'localhost'  # use the free broker from HIVEMQ
+    app.config['MQTT_BROKER_PORT'] = 1883  # default port for non-tls connection
+    app.config['MQTT_USERNAME'] = ''  # set the username here if you need authentication for the broker
+    app.config['MQTT_PASSWORD'] = ''  # set the password here if the broker demands authentication
+    app.config['MQTT_KEEPALIVE'] = 5  # set the time interval for sending a ping to the broker to 5 seconds
+    app.config['MQTT_TLS_ENABLED'] = False  # set TLS to disabled for testing purposes
+
+    global mqtt
+    mqtt = Mqtt(app)
+    global socketio
+    socketio = SocketIO(app, async_mode="eventlet")
+
+    return mqtt
+
+
+# Start MQTT publishing
+
+# Function that every second publishes a message
+def background_thread():
+    count = 0
+    while True:
+        time.sleep(1)
+        # Using app context is required because the get_status() functions
+        # requires access to the db.
+        with app.app_context():
+            message = json.dumps(status.get_status(), default=str)
+        # Publish
+        mqtt.publish('python/mqtt', message)
+
+
+# App will now have to be run with `python app.py` as flask is now wrapped in socketio.
+# The following makes sure that socketio is also used
+
+def run_socketio_app():
+    create_app()
+    create_mqtt_app()
+    socketio.run(app, host='localhost', port=5000, use_reloader=False, debug=True)
+
+
+if __name__ == '__main__':
+    run_socketio_app()
