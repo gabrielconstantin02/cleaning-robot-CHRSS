1b56bcf89ef7826e058bf5780f107ce577b0fab4
Sebastian Richiteanu <richiteanu.sebastian@gmail.com>
2022-02-01 10:56:53 -0600

    unit tests done

--- cleaning-robot/unit_test/test_cleaning_history.py
+++ cleaning-robot/unit_test/test_cleaning_history.py
@@ -5,4 +5,14 @@ import json
 import services.air_service as air_service
 import services.cleaning_history_service as cleaning_history_service
 
-# to do
+type = 0
+date = '01-02-2022'
+
+
+def test_get_cleaning_history(client):
+    request = client.get("/cleaning_history", data={"type": type, "date": date}, follow_redirects=True)
+    response = json.loads(request.data.decode())
+
+    assert request.status_code == 200
+    assert response["data"]["type"] == type
+    assert response["data"]["date"] == date
--- cleaning-robot/unit_test/test_cleaning_schedule.py
+++ cleaning-robot/unit_test/test_cleaning_schedule.py
@@ -11,7 +11,7 @@ random.seed(13)
 type = random.randint(0, 100000)
 date = '2022-02-01 15:59:46'
 
-def test_get_cleaning_schedule(client):
+def test_set_cleaning_schedule(client):
     request = client.post("/cleaning_schedule", data={"type": type, "date": date}, follow_redirects=True)
     response = json.loads(request.data.decode())
 
@@ -21,7 +21,7 @@ def test_get_cleaning_schedule(client):
     assert response["data"]["date"] == 'Tue, 01 Feb 2022 21:59:46 GMT'
 
 
-def test_get_cleaning_schedule_403_type(client):
+def test_set_cleaning_schedule_403_type(client):
     request = client.post("/cleaning_schedule", data={"type": "", "date": ""}, follow_redirects=True)
     response = json.loads(request.data.decode())
 
@@ -29,9 +29,17 @@ def test_get_cleaning_schedule_403_type(client):
     assert response["status"] == "Type is required."
 
 
-def test_get_cleaning_schedule_403_date(client):
+def test_set_cleaning_schedule_403_date(client):
     request = client.post("/cleaning_schedule", data={"type": type, "date": ""}, follow_redirects=True)
     response = json.loads(request.data.decode())
 
     assert request.status_code == 403
     assert response["status"] == "Date is required."
+
+
+def test_get_cleaning_schedule(client):
+    request = client.get("/cleaning_schedule", data={"type": type, "date": date}, follow_redirects=True)
+    response = json.loads(request.data.decode())
+
+    assert request.status_code == 200
+    assert response == date
