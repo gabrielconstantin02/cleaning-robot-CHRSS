9d8cc95e999ee89cf9a77359008a500cd7a23629
Daniel Sociu <danielsociu00@gmail.com>
2022-02-01 07:38:50 +0200

    Removed socketio connection handlers

--- cleaning-robot/app.py
+++ cleaning-robot/app.py
@@ -114,35 +114,6 @@ def run_socketio_app():
     create_mqtt_app()
     socketio.run(app, host='localhost', port=5000, use_reloader=False, debug=True)
 
-    @socketio.on('publish')
-    def handle_publish(json_str):
-        data = json.loads(json_str)
-        mqtt.publish(data['topic'], data['message'])
-
-
-    @socketio.on('subscribe')
-    def handle_subscribe(json_str):
-        data = json.loads(json_str)
-        mqtt.subscribe(data['topic'])
-
-
-    @socketio.on('unsubscribe_all')
-    def handle_unsubscribe_all():
-        mqtt.unsubscribe_all()
-
-
-    @mqtt.on_message()
-    def handle_mqtt_message(client, userdata, message):
-        data = dict(
-            topic=message.topic,
-            payload=message.payload.decode()
-        )
-        socketio.emit('mqtt_message', data=data)
-
-
-    @mqtt.on_log()
-    def handle_logging(client, userdata, level, buf):
-        print(level, buf)
 
 # If we run with python - this would imply to initialize the database manually
 if __name__ == '__main__':
