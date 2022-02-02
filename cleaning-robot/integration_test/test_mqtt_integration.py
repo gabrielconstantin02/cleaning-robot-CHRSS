34320aede8e9d03713454c4fb835b5dc66ee8364
Daniel Sociu <danielsociu00@gmail.com>
2022-02-02 13:04:38 +0200

    Fixed mqtt test integration

--- cleaning-robot/integration_test/test_mqtt_integration.py
+++ cleaning-robot/integration_test/test_mqtt_integration.py
@@ -7,6 +7,7 @@ from app import create_app, create_mqtt_app
 from paho.mqtt.client import (
     MQTT_ERR_SUCCESS,
 )
+
 app = None
 mqtt = None
 socketio = None
@@ -15,12 +16,13 @@ socketio = None
 def client_mqtt():
     app = create_app()
     mqtt = create_mqtt_app()
-    socketio = SocketIO(app, async_mode="eventlet")
-    socketio.run(app, host='localhost', port=5000, use_reloader=False, debug=True)
-    return mqtt
+    with app.test_client():
+        with app.app_context():
+            yield mqtt
 
 @pytest.mark.integrationTest
 def test_mqtt_status_publishing(client_mqtt):
     message = json.dumps(status.get_status(), default=str)
+    print(message)
     res, mid = client_mqtt.publish('cleaning-robot', message)
     assert res == MQTT_ERR_SUCCESS
